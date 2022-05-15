234437e7b1947b0aa3e6b74832062ce9a016f3e4
sun283 <asalia283@gmail.com>
2022-05-15 21:17:27 +0900

    first java study

---
+++ sortNameInOrder.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+  
+class GFG {
+    public static void main(String[] args)
+    {
+        // storing input in variable
+        int n = 4;
+        // create string array called names
+        String names[]
+            = { "Rahul", "Ajay", "Gourav", "Riya" };
+        // inbuilt sort function
+        Arrays.sort(names);
+        // print output array
+        System.out.println(
+            "The names in alphabetical order are: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
---
+++ sortNameInOrder3.java
@@ -0,0 +1,125 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main
+{
+   public static void main(String[] args)
+   {/* w  w  w.  j  a v  a 2  s.  co  m*/
+      Employee[] employees = {
+         new Employee("J", "Red", 51000, "IT"),
+         new Employee("A", "Green", 71600, "IT"),
+         new Employee("M", "Black", 35187.5, "Sales"),
+         new Employee("K", "Yellow", 47100.77, "Marketing"),
+         new Employee("L", "Pink", 62001, "IT"),
+         new Employee("D", "Blue", 32001, "Sales"),
+         new Employee("W", "Brown", 42361.4, "Marketing")};
+
+      // get List view of the Employees
+      List<Employee> list = Arrays.asList(employees);
+
+      // display all Employees
+      System.out.println("Complete Employee list:");
+      list.stream().forEach(System.out::println);
+      
+
+      // Functions for getting first and last names from an Employee
+      Function<Employee, String> byFirstName = Employee::getFirstName;
+      Function<Employee, String> byLastName = Employee::getLastName;
+
+      // Comparator for comparing Employees by first name then last name
+      Comparator<Employee> lastThenFirst = 
+         Comparator.comparing(byLastName).thenComparing(byFirstName);
+
+      // sort employees in descending order by last name, then first name
+      System.out.printf(
+         "%nEmployees in descending order by last name then first:%n");
+      list.stream()
+          .sorted(lastThenFirst.reversed())
+          .forEach(System.out::println);
+   } 
+}
+class Employee
+{
+   private String firstName;
+   private String lastName;
+   private double salary; 
+   private String department;
+   
+   // constructor 
+   public Employee(String firstName, String lastName, 
+      double salary, String department)
+   {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.salary = salary;
+      this.department = department;
+   } 
+
+   // set firstName
+   public void setFirstName(String firstName)
+   {
+      this.firstName = firstName;
+   }
+
+   // get firstName
+   public String getFirstName()
+   {
+      return firstName;
+   }
+
+   // set lastName
+   public void setLastName(String lastName)
+   {
+      this.lastName = lastName;
+   }
+
+   // get lastName
+   public String getLastName()
+   {
+      return lastName;
+   }
+
+   // set salary
+   public void setSalary(double salary)
+   {
+      this.salary = salary;
+   }
+
+   // get salary
+   public double getSalary()
+   {
+      return salary;
+   }
+
+   // set department
+   public void setDepartment(String department)
+   {
+      this.department = department;
+   }
+
+   // get department
+   public String getDepartment()
+   {
+      return department;
+   }
+
+   // return Employee's first and last name combined
+   public String getName()
+   {
+      return String.format("%s %s", getFirstName(), getLastName());
+   }
+
+   // return a String containing the Employee's information
+   @Override
+   public String toString() 
+   {
+      return String.format("%-8s %-8s %8.2f   %s", 
+         getFirstName(), getLastName(), getSalary(), getDepartment());
+   } 
+}
